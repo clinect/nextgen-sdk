5bd842e89cb4b11d0555cccdf261c83e84908315
Christian <christianf@codev.com>
2023-04-01 14:32:51 +0800

    added get all insurance payers and search insurance payer

---
+++ src/Requests/Insurances/GetAllInsurancePayer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Clinect\NextGen\Requests\Insurances;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetAllInsurancePayer extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        public array $args
+    ) {
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return "/master/payers";
+    }
+
+    protected function defaultQuery(): array
+    {
+        return $this->args;
+    }
+}
---
+++ src/Requests/Insurances/SearchInsurancePayer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Clinect\NextGen\Requests\Insurances;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class SearchInsurancePayer extends Request
+{
+    protected Method $method = Method::GET;
+    public array $query_params;
+
+    public function __construct(
+        public array $args
+    ) {
+        $this->query_params = '$filter=substringof(\'' . $args['name'] . '\', name)';
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return "/master/payers";
+    }
+
+    protected function defaultQuery(): array
+    {
+        return $this->query_params;
+    }
+}
