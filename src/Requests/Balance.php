afba6a87f3627f72fb75aa4385eca36c31bb4065
rceniza <rummelc@codev.com>
2023-04-01 08:10:59 +0800

    added patient balance request

---
+++ src/Requests/Balance.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Clinect\NextGen\Requests;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class Balance extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        public int $id
+    ) { }
+
+    public function resolveEndpoint(): string
+    {
+        return "/persons/{$this->id}/chart/balances";
+    }
+}
