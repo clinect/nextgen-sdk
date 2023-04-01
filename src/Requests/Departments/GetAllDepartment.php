b0807d46b3521c23b13cb806f02a481857945ff3
rceniza <rummelc@codev.com>
2023-04-01 10:15:11 +0800

    added department requests

---
+++ src/Requests/Departments/GetAllDepartment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Clinect\NextGen\Requests\Departments;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetAllDepartment extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        public int $practiceId
+    ) { }
+
+    public function resolveEndpoint(): string
+    {
+        return "/{$this->practiceId}/departments";
+    }
+}
