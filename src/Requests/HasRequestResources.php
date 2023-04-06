6eb35b1a567df0b5721914b16233e125f4547f1e
Christian <christianf@codev.com>
2023-04-07 03:59:03 +0800

    updated resources

--- src/Requests/HasRequestResources.php
+++ src/Requests/HasRequestResources.php
@@ -2,9 +2,11 @@
 
 namespace Clinect\NextGen\Requests;
 
+use Clinect\NextGen\Resources\AppointmentsResource;
 use Clinect\NextGen\Resources\PersonsResource;
 use Clinect\NextGen\Resources\PatientsResource;
 use Clinect\NextGen\Resources\DepartmentsResource;
+use Clinect\NextGen\Resources\HealthHistoryResource;
 
 trait HasRequestResources
 {
@@ -21,5 +23,15 @@ trait HasRequestResources
     public function departments(): DepartmentsResource
     {
         return new DepartmentsResource($this);
+    }
+
+    public function appointments(): AppointmentsResource
+    {
+        return new AppointmentsResource($this);
+    }
+
+    public function healthHistory(): HealthHistoryResource
+    {
+        return new HealthHistoryResource($this);
     }
 }
