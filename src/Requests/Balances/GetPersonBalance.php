b5fd9311b56d86dd8b0b989f4de0fc74336e992e
rceniza <rummelc@codev.com>
2023-04-06 17:01:57 +0800

    changed to person and added string as variable type

--- src/Requests/Balances/GetPersonBalance.php
+++ src/Requests/Balances/GetPersonBalance.php
@@ -5,12 +5,12 @@ namespace Clinect\NextGen\Requests\Balances;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 
-class GetPatientBalance extends Request
+class GetPersonBalance extends Request
 {
     protected Method $method = Method::GET;
 
     public function __construct(
-        public int $patientId
+        public int|string $patientId
     ) {
     }
 
