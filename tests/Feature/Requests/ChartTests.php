a456abcd7c797a12580ae4135ce3deedaafb3ee7
rceniza <rummelc@codev.com>
2023-04-07 21:45:14 +0800

    added chart tests

---
+++ tests/Feature/Requests/ChartTests.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Clinect\NextGen\Tests\Feature\Requests;
+
+use Clinect\NextGen\NextGen;
+use Orchestra\Testbench\TestCase;
+use Clinect\NextGen\Requests\Chart;
+use Clinect\NextGen\Tests\Stubs\Chart as ChartStub;
+
+class ChartTests extends TestCase
+{
+    use ChartStub;
+
+    public function testCanSeeAllCharts()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new Chart)->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 200);
+
+        foreach ($response->json() as $key => $result) {
+            $this->assertSame($this->all()[$key]['name'], $result['name']);
+            $this->assertSame($this->all()[$key]['category'], $result['category']);
+        }
+    }
+
+    public function testCanSeeChart()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new Chart('id-3'))->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 200);
+        $this->assertSame($response->json('name'), 'Chart 3');
+        $this->assertSame($response->json('category'), 'chart-3');
+    }
+
+    public function testAppointmentNotFound()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new Chart('id-4'))->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 404);
+        $this->assertSame($response->json('error'), 'No data available');
+    }
+}
