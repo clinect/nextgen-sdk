a3ef17f2a7b783fd65f44c4be1a66893f60f8d37
rceniza <rummelc@codev.com>
2023-04-07 21:44:42 +0800

    added a request for get

---
+++ src/Requests/BaseRequests/GetRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Clinect\NextGen\Requests\BaseRequests;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        public string $endpoint,
+        public array $queries = [],
+        public array $configs = []
+    ) {
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return $this->endpoint;
+    }
+
+    protected function defaultQuery(): array
+    {
+        return $this->queries;
+    }
+
+    protected function defaultConfig(): array
+    {
+        return $this->configs;
+    }
+}
