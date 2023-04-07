477dc6af89b5df3744f22903136912405a55d855
rceniza <rummelc@codev.com>
2023-04-07 21:45:05 +0800

    added appointment tests

---
+++ tests/Feature/Requests/AppointmentTests.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Clinect\NextGen\Tests\Feature\Requests;
+
+use Clinect\NextGen\NextGen;
+use Orchestra\Testbench\TestCase;
+use Clinect\NextGen\Requests\Appointment;
+use Clinect\NextGen\Tests\Stubs\Appointment as AppointmentStub;
+
+class AppointmentTests extends TestCase
+{
+    use AppointmentStub;
+
+    public function testCanSeeAllAppointments()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new Appointment)->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 200);
+
+        foreach ($response->json() as $key => $result) {
+            $this->assertSame($this->all()[$key]['name'], $result['name']);
+            $this->assertSame($this->all()[$key]['category'], $result['category']);
+        }
+    }
+
+    public function testCanSeeAppointment()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new Appointment('id-3'))->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 200);
+        $this->assertSame($response->json('name'), 'Appointment 3');
+        $this->assertSame($response->json('category'), 'appointment-3');
+    }
+
+    public function testAppointmentNotFound()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new Appointment('id-4'))->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 404);
+        $this->assertSame($response->json('error'), 'No data available');
+    }
+}
