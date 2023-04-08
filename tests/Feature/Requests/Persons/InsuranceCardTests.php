7fe797342f3bc583761fe3f73349eebeef4a869b
rceniza <rummelc@codev.com>
2023-04-08 11:05:51 +0800

    added person insurance card tests

---
+++ tests/Feature/Requests/Persons/InsuranceCardTests.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Clinect\NextGen\Tests\Feature\Requests\Persons;
+
+use Clinect\NextGen\NextGen;
+use Orchestra\Testbench\TestCase;
+use Clinect\NextGen\Requests\PersonRequests;
+use Clinect\NextGen\Tests\Stubs\Person as PersonStub;
+
+class InsuranceCardTests extends TestCase
+{
+    use PersonStub;
+
+    public function testCanSeeAllInsuranceCards()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new PersonRequests('person-id'))
+            ->insurances('insurance-id')
+            ->cards()
+            ->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 200);
+
+        foreach ($response->json() as $key => $result) {
+            $this->assertSame($this->insuranceCards()[$key]['name'], $result['name']);
+            $this->assertSame($this->insuranceCards()[$key]['category'], $result['category']);
+        }
+    }
+
+    public function testCanSeeInsuranceCard()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new PersonRequests('person-id'))
+            ->insurances('insurance-id')
+            ->cards('id-3')
+            ->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 200);
+        $this->assertSame($response->json('name'), 'Person insurance card 3');
+        $this->assertSame($response->json('category'), 'person-insurance-card-3');
+    }
+
+    public function testInsuranceCardNotFound()
+    {
+        $baseUrl = 'test.clinect.com';
+
+        $connector = new NextGen(baseUrl: $baseUrl);
+
+        $connector->withMockClient($this->client($baseUrl));
+
+        $request = (new PersonRequests('person-id'))
+            ->insurances('insurance-id')
+            ->cards('id-4')
+            ->get();
+
+        $response = $connector->send($request);
+
+        $this->assertSame($response->status(), 404);
+        $this->assertSame($response->json('error'), 'No data available');
+    }
+}
