02ce7ec49958c51fa97be69fc898ea6e470d585a
rceniza <rummelc@codev.com>
2023-04-12 21:14:37 +0800

    added authorize method, still wip

--- src/NextGen.php
+++ src/NextGen.php
@@ -4,14 +4,14 @@ namespace Clinect\NextGen;
 
 use Saloon\Http\Request;
 use Saloon\Http\Connector;
+use Clinect\NextGen\Requests\AuthRequest;
 use Saloon\Http\Paginators\PagedPaginator;
+use Clinect\NextGen\Requests\NgSessionRequest;
 use Clinect\NextGen\Requests\RequestResources;
 
 class NextGen extends Connector
 {
     use RequestResources;
-
-    public int $perPage = 20;
 
     public function __construct(
         public string $clientId = '',
@@ -19,28 +19,63 @@ class NextGen extends Connector
         public string $siteId = '',
         public string $enterpriseId = '',
         public string $practiceId = '',
-        public string $baseUrl = '',
-        public string $routeUri = ''
-    ) { }
-    
+        public string $baseUrl = 'https://nativeapi.nextgen.com/nge/prod',
+        public string $routeUri = '/nge-api/api',
+    ) {
+        $this->authorize();
+    }
+
     public function resolveBaseUrl(): string
     {
-        return $this->baseUrl;
+        return "{$this->baseUrl}{$this->routeUri}";
     }
 
     protected function defaultHeaders(): array
     {
-        return [
-            'Accept' => 'application/json',
-            'Content-Type' => 'application/json',
-        ];
+        return [];
     }
 
-    protected function defaultQuery(): array
+    protected function authorize(): void
     {
-        return [
-            'per_page' => $this->perPage,
-        ];
+        $routeUri = $this->routeUri;
+
+        $this->routeUri = '';
+
+        $request = (new AuthRequest)
+            ->fill([
+                'grant_type' => 'client_credentials',
+                'client_id' => $this->clientId,
+                'client_secret' => $this->secret,
+                'site_id' => $this->siteId,
+            ])
+            ->post();
+
+        $response = $this->send($request);
+
+        $this->routeUri = $routeUri;
+
+        if ($response->failed()) {
+            $response->throw();
+        }
+
+        $this->withTokenAuth((string) $response->json('access_token'), (string) $response->json('token_type'));
+
+        $request = (new NgSessionRequest)
+            ->withConfig([
+                'json' => [
+                    'enterpriseid' => $this->enterpriseId,
+                    'practiceId' => $this->practiceId,
+                ],
+            ])
+            ->put();
+
+        $response = $this->send($request);
+
+        if ($response->failed()) {
+            $response->throw();
+        }
+
+        $this->headers()->add('X-NG-SessionID', $response->headers()->get('x-ng-sessionid'));
     }
 
     public function paginate(Request $request, int $perPage = 20): PagedPaginator
