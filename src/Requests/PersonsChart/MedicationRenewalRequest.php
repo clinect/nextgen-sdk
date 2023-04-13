53b61c0e3df40eae72d41a46a770cf1e74f3d4aa
Christian <christianf@codev.com>
2023-04-13 21:40:49 +0800

    added medications and obstetrics related requests

---
+++ src/Requests/PersonsChart/MedicationRenewalRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Clinect\NextGen\Requests\PersonsChart;
+
+use Clinect\NextGen\Requests\Request;
+
+class MedicationRenewalRequest extends Request
+{
+    public function __construct(
+        public string $endPoint,
+        public int|string|null $id
+    ) {
+        $this->withUriParamId($id);
+    }
+
+    public function defaultEndpoint(): string
+    {
+        return $this->endPoint . '/medication-renewal-requests';
+    }
+
+    public function medications(): static
+    {
+        return $this->addEndpoint('/medications');
+    }
+}
