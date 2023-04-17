c61d536f8a488e7c7526655112d7e71fd9c33179
Christian <christianf@codev.com>
2023-04-18 07:40:21 +0800

    updated favorites request

--- src/Requests/FavoritesRequest.php
+++ src/Requests/FavoritesRequest.php
@@ -3,6 +3,7 @@
 namespace Clinect\NextGen\Requests;
 
 use Clinect\NextGen\Requests\Favorites\MedicationsRequest;
+use Clinect\NextGen\Requests\Users\DiagnosesGroupsRequest;
 
 class FavoritesRequest extends Request
 {
@@ -15,6 +16,17 @@ class FavoritesRequest extends Request
     {
         $this->cleanUpEndpoint();
         return new MedicationsRequest($this->endpoint, $id);
+    }
+
+    public function diagnosesGroups(int|string|null $id): DiagnosesGroupsRequest
+    {
+        $this->cleanUpEndpoint();
+        return new DiagnosesGroupsRequest($this->endpoint, $id);
+    }
+
+    public function procedures()
+    {
+        return $this->addEndpoint('/procedures');
     }
 }
  
