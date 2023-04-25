e2f349bedb12e19b05254625e5dce8fb8e25e567
Christian <christianf@codev.com>
2023-04-26 07:55:28 +0800

    added appointment tests using fixtures, some using mock response

---
+++ tests/Feature/Connector/Requests/AppointmentsTests.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Clinect\NextGen\Tests\Feature\Connector\Requests;
+
+use Clinect\NextGen\NextGen;
+use Clinect\NextGen\Tests\Feature\TestCase;
+use Clinect\NextGen\Tests\Stubs\Appointment as AppointmentStub;
+
+class AppointmentsTests extends TestCase
+{
+    use AppointmentStub;
+    private $apiConnector;
+    private $mockConnector;
+
+    protected function setup(): void
+    {
+        parent::setup();
+        $this->apiConnector = new NextGen($this->apiConfig(), $this->fixtureClient());
+        $this->mockConnector = new NextGen($this->mockConfig(), $this->mockClient());
+    }
+
+    public function testCanSeeAllAppointments()
+    {
+        $request = $this->apiConnector->disableCaching()->appointments()->get();
+       
+        $response = $this->apiConnector->send($request);
+        $this->assertSame($response->status(), 200);
+        $this->assertArrayHasKey('items',$response->json());
+        $this->assertNotEmpty($response->json()['items']);
+    }
+
+    public function testCanSeeAppointmentsSlot()
+    {
+        $request = $this->apiConnector->disableCaching()->appointments()->slots()->get();
+        $response = $this->apiConnector->send($request);
+
+        $this->assertSame($response->status(), 200);
+        $this->assertArrayHasKey('items', $response->json());
+    }
+
+    public function testCanSeeAppointmentsWaitListItems()
+    {
+        $request = $this->apiConnector->disableCaching()->appointments()->waitlistItems()->get();
+        $response = $this->apiConnector->send($request);
+
+        $this->assertSame($response->status(), 200);
+        $this->assertArrayHasKey('items', $response->json());
+    }
+
+    public function testCanSeeAppointment()
+    {
+        $appointmentId = "97456e4b-8575-4423-857e-ba7549d65d25";
+        $request = $this->apiConnector->appointments($appointmentId)->get();
+
+        $response = $this->apiConnector->send($request);
+
+        $this->assertSame($response->status(), 200);
+        $this->assertSame($response->json()['id'], $appointmentId);
+        
+    }
+
+    public function testCanSeeAppointmentResponse()
+    {
+        $request = $this->mockConnector->appointments()->responses(1)->get();
+
+        $response = $this->mockConnector->send($request);
+
+        $this->assertSame($response->status(), 200);
+        $this->assertSame($response->json('name'), 'Appointment Response 1');
+        $this->assertSame($response->json('category'), 'appointment-response-1');
+    }
+
+    public function testCanSeeAppointmentsWaitListItem()
+    {
+        $request = $this->mockConnector->appointments()->waitlistItems(1)->get();
+
+        $response = $this->mockConnector->send($request);
+
+        $this->assertSame($response->status(), 200);
+        $this->assertSame($response->json('name'), 'Appointment waitlist 1');
+        $this->assertSame($response->json('category'), 'appointment-waitlist-1');
+    }
+
+    public function testAppointmentNotFound()
+    {
+        $request = $this->apiConnector->appointments('id-4')->get();
+
+        $response = $this->apiConnector->send($request);
+
+        $this->assertSame($response->status(), 400);
+        $this->assertSame($response->json('message'), "The value 'id-4' is not valid for Guid.");
+    }
+    
+}
