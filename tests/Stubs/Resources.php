b255840cc0b0670f52945f3653b246730b1be7a1
Christian <christianf@codev.com>
2023-05-05 00:22:02 +0800

    added resources fixtures, tests, and stub, fixed wrong endpoints

--- tests/Stubs/Resources.php
+++ tests/Stubs/Resources.php
@@ -9,15 +9,18 @@ trait Resources
 {
     private $apiConnector;
     private $mockConnector;
+    private $resourceId = "1984e9e2-df9c-4a91-a2e4-10f6af2a3305";
 
     protected function mockClient(): MockClient
     {
         $response = [
             ...$this->mockAuthorize(),
             ...[
-                "{$this->url()}/resources" => MockResponse::make([
-                    'name' => 'Resources 1',
-                    'category' => 'resources-1',
+                "{$this->url()}/resources/{$this->resourceId}/appointments/1" => MockResponse::make([
+                    'name' => 'appointment 1',
+                ], 200),
+                "{$this->url()}/resources/{$this->resourceId}/appointments/1/status-histories" => MockResponse::make([
+                    'name' => 'appointment status-histories',
                 ], 200),
             ],
         ];
@@ -32,10 +35,10 @@ trait Resources
             ...$this->apiAuthorize(),
             ...[
                 "{$this->apiUrl()}/resources" => MockResponse::fixture('Resources/resources'),
-                "{$this->apiUrl()}/resources/1" => MockResponse::fixture('Resources/resource'),
-                "{$this->apiUrl()}/resources/1/appointments" => MockResponse::fixture('Resources/appointments'),
-                "{$this->apiUrl()}/resources/1/appointments/1" => MockResponse::fixture('Resources/appointment'),
-                "{$this->apiUrl()}/resources/1/appointments/1/status-histories" => MockResponse::fixture('Resources/statusHistories'),
+                "{$this->apiUrl()}/resources/{$this->resourceId}" => MockResponse::fixture('Resources/resource'),
+                "{$this->apiUrl()}/resources/{$this->resourceId}/appointments" => MockResponse::fixture('Resources/appointments'),
+                "{$this->apiUrl()}/resources/{$this->resourceId}/appointments/1" => MockResponse::fixture('Resources/appointment'),
+                "{$this->apiUrl()}/resources/{$this->resourceId}/appointments/1/status-histories" => MockResponse::fixture('Resources/statusHistories'),
             ],
         ];
 
